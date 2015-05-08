b08ce01609c67eaaf6228b32d6b9b9bfb2d0f25c
Deok Seong Kim <Musicalapple@Giui-iMac.local>
2015-05-08 20:59:29 +0900

    check wow.js is not working with skrollr.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-var $bulletin = $('#bulletin');
-// initialize
-$bulletin.masonry({
-    "columnWidth": ".grid-sizer",
-    "itemSelector": ".bulletin-item",
-    "percentPosition": true
-});
+$(window).load(function(){
+    var $bulletin = $('#bulletin');
 
+    // initialize
+    $bulletin.masonry({
+        "columnWidth": ".grid-sizer",
+        "itemSelector": ".bulletin-item",
+        "percentPosition": true
+    });
+});
 new WOW().init();
-skrollr.init();
+skrollr.init({forceHeight: false});
